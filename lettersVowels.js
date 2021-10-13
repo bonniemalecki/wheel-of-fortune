6f441bf674e93be3a91e85f773606f68fd0cdb2c
BrennanM8 <91911004+BrennanM8@users.noreply.github.com>
2021-10-13 15:14:05 -0400

    Add files via upload

---
+++ lettersVowels.js
@@ -0,0 +1,22 @@
+import sys
+import random
+import time
+
+Letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.lower()
+Vowels = 'A' 'E' 'I' 'O' 'U'
+Vowel_Cost  = 250
+
+if Vowels == 'A'
+    Vowel_Cost = 250
+elif Vowels == 'E':
+    Vowel_Cost == 250
+elif Vowels == 'I':
+    Vowel_Cost == 250
+elif Vowels == 'O':
+    Vowel_Cost== 250
+elif  Vowels == 'U':
+    Vowel_Cost == 250
+
+
+}
+
